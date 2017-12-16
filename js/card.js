619aa3ba6166060a58204ac1d0edba6900a24245
Deilan <deilan@mail.ru>
2017-12-16 20:34:18 +0300

    Update existing card instead of recreating it on change of active map pin

--- js/card.js
+++ js/card.js
@@ -10,11 +10,8 @@
   };
 
   window.showAdMapCard = function (els, ad) {
-    var mapCard = els.map.querySelector('.map__card');
-    if (mapCard) {
-      mapCard.parentElement.removeChild(mapCard);
-    }
-    mapCard = initMapCardEl(els.template.cloneNode(true), ad);
+    var mapCard = els.map.querySelector('.map__card') || els.template.cloneNode(true);
+    initMapCardEl(mapCard, ad);
     document.addEventListener('keydown', onEscClick);
     els.mapFiltersContainer.insertAdjacentElement('beforebegin', mapCard);
   };
@@ -45,22 +42,22 @@
     }
   }
 
-  function initMapCardEl(mapCardEl, ad) {
-    mapCardEl.querySelector('.popup__avatar').setAttribute('src', ad.author.avatar);
-    mapCardEl.querySelector('h3').textContent = ad.offer.title;
-    mapCardEl.querySelector('p small').textContent = ad.offer.address;
-    mapCardEl.querySelector('.popup__price').innerHTML = ad.offer.price + '&#x20bd;/ночь';
-    mapCardEl.querySelector('h4').textContent = TYPES_DICTIONARY[ad.offer.type];
-    mapCardEl.querySelector('h4 + p').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
-    mapCardEl.querySelector('h4 + p + p').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
-    var featuresListEl = mapCardEl.querySelector('.popup__features');
+  function initMapCardEl(mapCard, ad) {
+    mapCard.querySelector('.popup__avatar').src = ad.author.avatar;
+    mapCard.querySelector('h3').textContent = ad.offer.title;
+    mapCard.querySelector('p small').textContent = ad.offer.address;
+    mapCard.querySelector('.popup__price').innerHTML = ad.offer.price + '&#x20bd;/ночь';
+    mapCard.querySelector('h4').textContent = TYPES_DICTIONARY[ad.offer.type];
+    mapCard.querySelector('h4 + p').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+    mapCard.querySelector('h4 + p + p').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+    var featuresListEl = mapCard.querySelector('.popup__features');
     initFeaturesListEl(featuresListEl, ad.offer.features);
-    mapCardEl.querySelector('.popup__features + p').textContent = ad.offer.description;
-    var closeBtn = mapCardEl.querySelector('.popup__close');
+    mapCard.querySelector('.popup__features + p').textContent = ad.offer.description;
+    var closeBtn = mapCard.querySelector('.popup__close');
     if (closeBtn) {
       closeBtn.addEventListener('click', onClickCloseMapCard);
     }
-    return mapCardEl;
+    return mapCard;
   }
 
   function initFeaturesListEl(featuresListEl, features) {
@@ -69,11 +66,8 @@
       var feature = features.find(function (feat) {
         return featuresListItemEls[i].classList.contains('feature--' + feat);
       });
-      if (!feature) {
-        featuresListEl.removeChild(featuresListItemEls[i]);
-      }
+      featuresListItemEls[i].style.display = feature ? null : 'none';
     }
-    return featuresListEl;
   }
 
 })();
